365277e28efba307e6d9b67422f9745df3a12868
EngrMonyrul <121379927+EngrMonyrul@users.noreply.github.com>
2023-11-05 09:35:58 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+import 'functions/barcode_scanner_view.dart';
+import 'functions/digital_link_detector_view.dart';
+import 'functions/entity_extraction_view.dart';
+import 'functions/face_detector_view.dart';
+import 'functions/face_mesh_detector_view.dart';
+import 'functions/label_detector.dart';
+import 'functions/language_identifier_view.dart';
+import 'functions/language_translator_view.dart';
+import 'functions/object_detector_view.dart';
+import 'functions/pose_detector_view.dart';
+import 'functions/selfie_segment_view.dart';
+import 'functions/smart_replay_view.dart';
+import 'functions/text_detector_view.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google ML Kit Demo App'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  ExpansionTile(
+                    title: const Text('Vision APIs'),
+                    children: [
+                      CustomCard('Barcode Scanning', BarcodeScannerView()),
+                      CustomCard('Face Detection', FaceDetectorView()),
+                      CustomCard('Face Mesh Detection', FaceMeshDetectorView()),
+                      CustomCard('Image Labeling', ImageLabelView()),
+                      CustomCard('Object Detection', ObjectDetectorView()),
+                      CustomCard('Text Recognition', TextRecognizerView()),
+                      CustomCard('Digital Ink Recognition', DigitalInkView()),
+                      CustomCard('Pose Detection', PoseDetectorView()),
+                      CustomCard('Selfie Segmentation', SelfieSegmenterView()),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ExpansionTile(
+                    title: const Text('Natural Language APIs'),
+                    children: [
+                      CustomCard('Language ID', LanguageIdentifierView()),
+                      CustomCard(
+                          'On-device Translation', LanguageTranslatorView()),
+                      CustomCard('Smart Reply', SmartReplyView()),
+                      CustomCard('Entity Extraction', EntityExtractionView()),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomCard extends StatelessWidget {
+  final String _label;
+  final Widget _viewPage;
+  final bool featureCompleted;
+
+  const CustomCard(this._label, this._viewPage, {this.featureCompleted = true});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.only(bottom: 10),
+      child: ListTile(
+        tileColor: Theme.of(context).primaryColor,
+        title: Text(
+          _label,
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        onTap: () {
+          if (!featureCompleted) {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content:
+                const Text('This feature has not been implemented yet')));
+          } else {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => _viewPage));
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/object_detection.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ObjectDetectionMLKit extends StatefulWidget {
+  const ObjectDetectionMLKit({super.key});
+
+  @override
+  State<ObjectDetectionMLKit> createState() => _ObjectDetectionMLKitState();
+}
+
+class _ObjectDetectionMLKitState extends State<ObjectDetectionMLKit> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
